1748e2eb18cad17e0ce09fa78f4c5e828b5a1c9f
selvakumaran-mns <selvakumaran.ramachandran@mnscorp.net>
2024-02-13 06:06:45 +0530

    TDDStringCalculator return number number without any specail char

--- src/main/java/org/springaop/stringCalculator.java
+++ src/main/java/org/springaop/stringCalculator.java
@@ -2,7 +2,7 @@ package org.springaop;
 
 public class stringCalculator {
     public static int addStringNumbers(String numbers){
-
+        if(numbers != null && !numbers.isEmpty()) return Integer.parseInt(numbers);
         return 0;
     }
 }
